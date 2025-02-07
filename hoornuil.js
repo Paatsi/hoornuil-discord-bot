9e504e4946e28d819b5c19bdc84da76c168da8cd
Paatsi <paatsidev@gmail.com>
2025-02-07 23:36:32 +0200

    Added openweathermap api data fetch by zipcode

--- hoornuil.js
+++ hoornuil.js
@@ -1,21 +1,55 @@
-// Initialize dotenv
-require('dotenv').config();
+require('dotenv').config()
+const fetch = require('node-fetch')
+const countryCode = 'FI'
 
-// Discord.js versions ^13.0 require us to explicitly define client intents
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents, Message } = require('discord.js')
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-   });
+	console.log(`Logged in as ${client.user.tag}!`)
+})
 
-// Log In our bot
-client.login(process.env.CLIENT_TOKEN);
+client.login(process.env.CLIENT_TOKEN)
 
-
-client.on('messageCreate', msg => {
-    // You can view the msg object here with console.log(msg)
-     if (msg.content === 'Huhuu!') {
-       msg.reply(`Huhuu ${msg.author.username}` + '!');
-     }
-    });
+client.on('messageCreate', (msg) => {
+	if (msg.content.includes('!w') && !msg.author.bot) {
+		const zipCode = msg.content.split(' ')[1]
+		if (
+			zipCode === undefined ||
+			zipCode.length != 5 ||
+			parseInt(zipCode) === NaN
+		) {
+			msg.channel.send('Tarkista postinumero!').catch(console.error)
+			return
+		} else {
+			fetch(
+				`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&units=metric&appid=${process.env.OPENWEATHERMAP_TOKEN}`
+			)
+				.then((response) => {
+					return response.json()
+				})
+				.then((parsedWeather) => {
+					if (parsedWeather.cod === '404') {
+						msg.channel.send(
+							'`Postinumeroa ei ole olemassa tai tietoa ei ole saatavilla!`'
+						)
+					} else {
+						const current = new Date()
+						msg.channel.send(
+							`
+              Tämänhetkinen sää:
+              Paikkakunta: ${parsedWeather.name}, ${parsedWeather.sys.country}
+              Sääennuste:  ${parsedWeather.weather[0].main}
+              Lämpötila:   ${parsedWeather.main.temp.toFixed(1)} °C
+              Korkein lämpötila: ${parsedWeather.main.temp_max.toFixed(1)} °C
+              Alin lämpötila: ${parsedWeather.main.temp_min.toFixed(1)} °C
+              Kellonaika: ${current.toLocaleTimeString()}
+              `
+						)
+					}
+				})
+		}
+	}
+})
